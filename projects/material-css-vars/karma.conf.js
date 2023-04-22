8663b3ce31e00a90b07014f7f402e7968f5256ba
Daniel Kimmich <json-derulo@users.noreply.github.com>
2023-04-22 12:49:18 +0200

    test: basic setup of lib tests

--- projects/material-css-vars/karma.conf.js
+++ projects/material-css-vars/karma.conf.js
@@ -7,18 +7,21 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
+      require('karma-coverage'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
       dir: require('path').join(__dirname, '../../coverage/material-css-vars'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' },
+      ],
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
--- projects/material-css-vars/src/lib/material-css-vars.service.spec.ts
+++ projects/material-css-vars/src/lib/material-css-vars.service.spec.ts
@@ -1,12 +1,51 @@
 import {TestBed} from '@angular/core/testing';
 
 import {MaterialCssVarsService} from './material-css-vars.service';
+import {MATERIAL_CSS_VARS_CFG} from '../mat-css-config-token.const';
 
 describe('MaterialCssVarsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: MaterialCssVarsService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: MATERIAL_CSS_VARS_CFG,
+          useValue: {
+            primary: '#123456',
+            accent: '#654321',
+            warn: '#FF0000',
+            isDarkTheme: true,
+          },
+        }
+      ],
+    });
+    service = TestBed.inject(MaterialCssVarsService);
+  });
 
   it('should be created', () => {
-    const service: MaterialCssVarsService = TestBed.get(MaterialCssVarsService);
     expect(service).toBeTruthy();
+  });
+
+  describe('constructor', () => {
+    it('should apply the config from injection token', () => {
+      expect(service.cfg.primary).toEqual('#123456');
+    });
+
+    it('should set default values', () => {
+      expect(service.cfg.darkThemeClass).toEqual('isDarkTheme');
+    });
+
+    it('should set accent color from injection token config', () => {
+      expect(service.accent).toEqual('#654321');
+    });
+
+    it('should set warn color from injection token config', () => {
+      expect(service.warn).toEqual('#FF0000');
+    });
+
+    it('should apply dark mode from injection token config', () => {
+      expect(service.isDarkTheme).toBeTrue();
+    });
   });
 });
