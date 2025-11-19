23f450b05b31aef2f06c3de0693cc9e90dea95b4
Daniel Kimmich <json-derulo@users.noreply.github.com>
2025-11-19 21:44:33 +0100

    chore(deps)!: update to angular v21

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,14 @@
-import { ApplicationConfig } from "@angular/core";
+import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
 import { provideMaterialCssVars } from "../../projects/material-css-vars/src/lib/material-css-vars.module";
 import { APP_BASE_HREF } from "@angular/common";
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZoneChangeDetection(),
     provideMaterialCssVars({
       primary: "#3f51b5",
       accent: "#e91e63",
       warn: "#f44336",
-      rootSelector: "app-root",
     }),
     { provide: APP_BASE_HREF, useValue: window._app_base ?? "/" },
   ],
